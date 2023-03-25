19ec0f44c191724f3a7bff02769f0c6e8368b60f
Artyom Leonov <09leonov@gmail.com>
2023-03-25 18:08:12 +0200

    Fix for github pages

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/home' element={<Home/>}/>
+        <Route path='/teender' element={<Home/>}/>
         <Route path='/login' element={<Login/>}/>
         <Route path='/register' element={<Register/>}/>
         <Route path='/' element={<Home/>}/>
