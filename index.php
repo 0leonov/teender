f08ee18cae4fdc916b367edbf4eb0e59d788371f
Artyom Leonov <61983418+0leonov@users.noreply.github.com>
2023-03-28 11:21:53 +0300

    Update index.php

--- index.php
+++ index.php
@@ -3,8 +3,22 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
 
 include 'DbConnect.php';
-$objDb = new DbConnect;
-$conn = $objDb->connect();
+$db = new DbConnect;
+$conn = $db->connect();
+
+function usernameExists($conn, $username) {
+    $sql = "SELECT * FROM users u WHERE u.username=:username;";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute(array(':username' => $username));
+    return !!$stmt->fetch(PDO::FETCH_ASSOC);
+}
+
+function emailExists($conn, $email) {
+    $sql = "SELECT * FROM users u WHERE u.email=:email;";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute(array(':email' => $email));
+    return !!$stmt->fetch(PDO::FETCH_ASSOC);
+}
 
 $method = $_SERVER['REQUEST_METHOD'];
 switch($method) {
@@ -17,15 +31,28 @@ switch($method) {
         break;
     case "POST":
         $user = json_decode(file_get_contents('php://input'));
+
+        if (usernameExists($conn, $user->username)) {
+            $response = json_encode(['status' => 0, 'error' => 'Username already taken']);
+            echo $response;
+            break;
+        }
+
+        if (emailExists($conn, $user->email)) {
+            $response = json_encode(['status' => 0, 'error' => 'Email already taken']);
+            echo $response;
+            break;
+        }
+
         $sql = "INSERT INTO users(id, email, username, password) VALUES(null, :email, :username, :password)";
         $stmt = $conn->prepare($sql);
         $stmt->bindParam(':email', $user->email);
         $stmt->bindParam(':username', $user->username);
         $stmt->bindParam(':password', $user->password);
         if ($stmt->execute()) {
-            $response = json_encode(['status' => 1, 'message' => 'Record created successfully.']);
+            $response = json_encode(['status' => 1]);
         } else {
-            $response = json_encode(['status' => 0, 'message' => 'Something went wrong!']);
+            $response = json_encode(['status' => 0, 'error' => 'Something went wrong']);
         }
         echo $response;
         break;
