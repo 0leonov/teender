340128ee674f85d382f58be8113369fb26421b53
23DPAPIMA <103630425+13DPAPIMA@users.noreply.github.com>
2023-05-21 19:28:43 +0300

    Update AgreeCheckbox.jsx

--- src/components/auth/AgreeCheckbox.jsx
+++ src/components/auth/AgreeCheckbox.jsx
@@ -1,24 +1,22 @@
-import { Link } from 'react-router-dom'
-
 const AgreeCheckbox = ({ name, rules, register, error }) => {
+
+
   return (
     <div className='flex gap-2'>
-      <input type='checkbox' className={`checkbox ${error ? 'checkbox-error' : 'checkbox-primary'}`} {...register(name, rules)} />
+      <input type='checkbox'
+             className={`checkbox ${error ? 'checkbox-error' : 'checkbox-primary'}`} {...register(name, rules)} />
 
       <span className='label-text'>
-        I agree with the{' '}
-        <Link to='terms' className='link link-primary'>
-          Terms
-        </Link>{' '}
+      I agree with the{' '}
+        <a href="/terms" id="section1" className='link link-primary'>
+        Terms
+      </a>{' '}
         and{' '}
-        <Link to='terms' className='link link-primary'>
-          {' '}
-          Conditions
-        </Link>
-        .
-      </span>
+        <a href="/terms" id="section2" className='link link-primary'>
+        Rules
+      </a>
+    </span>
     </div>
   )
 }
-
 export default AgreeCheckbox
