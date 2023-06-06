558c31cdebe303f3a7f4c123fbee8b59b24154e6
Fēlikss <103173146+21DP3FRegz@users.noreply.github.com>
2023-06-06 09:36:21 +0300

    Update getPhoto.php

--- user/getPhoto.php
+++ user/getPhoto.php
@@ -1,10 +1,13 @@
 <?php
 
-function getPhoto($conn, $userId): string
+function getPhoto($conn, $userId)
 {
     $statement = $conn->prepare("SELECT photo FROM users WHERE id = ?");
     $statement->bindParam(1, $userId, PDO::PARAM_INT);
     $statement->execute();
     $photoData = $statement->fetch(PDO::FETCH_COLUMN);
+    if (empty($photoData)) {
+        return;
+    }
     return base64_encode($photoData);
 }
